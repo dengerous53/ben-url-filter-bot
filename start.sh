2566db2d4167af41a4d7c2646cc4d5cd5bf79865
Dengerous <99312935+dengerous53@users.noreply.github.com>
2023-02-25 20:20:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/Moviesbotz/ben-url-filter-bot.git /ben-url-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /ben-url-filter-bot
+  git clone https://github.com/dengerous53/ben-url-filter-bot /ben-url-filter-bot
 fi
 cd /ben-url-filter-bot
 pip3 install -U -r requirements.txt
